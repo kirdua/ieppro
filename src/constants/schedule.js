704b125163ecd6ac76959bf6ed06127471f6a83e
kirdua <kirdua@gmail.com>
2026-04-24 14:46:26 -0500

    bug fix

---
+++ src/constants/schedule.js
@@ -0,0 +1,22 @@
+export const COLOR_OPTIONS = [
+  'blue',
+  'red',
+  'green',
+  'purple',
+  'teal',
+  'orange',
+  'indigo',
+  'pink',
+  'brown',
+  'grey'
+]
+
+export const DAY_OPTIONS = [
+  { label: 'Sunday', value: 'sun' },
+  { label: 'Monday', value: 'mon' },
+  { label: 'Tuesday', value: 'tue' },
+  { label: 'Wednesday', value: 'wed' },
+  { label: 'Thursday', value: 'thu' },
+  { label: 'Friday', value: 'fri' },
+  { label: 'Saturday', value: 'sat' }
+]
